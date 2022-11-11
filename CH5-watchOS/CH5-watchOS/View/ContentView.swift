877da18fb429f0a201a4a7797da58b3632a797d8
Kiara Medeiros <kiaramedeiros2001@gmail.com>
2022-11-11 08:52:03 -0400

    chore: Added HomeView

--- CH5-watchOS/CH5-watchOS/View/ContentView.swift
+++ CH5-watchOS/CH5-watchOS/View/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  CH5-watchOS
+//  CH5-watchOS Watch App
 //
 //  Created by Luciano Uchoa on 07/11/22.
 //
@@ -8,66 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Binding var routine: [RoutineInfo]
-    
     var body: some View {
         VStack {
-            HStack {
-                Text("Vamos cuidar da sua pele?")
-                    .font(.system(size: 24))
-                    .fontWeight(.black)
-                    .foregroundColor(.titleColor)
-                    //.padding([.horizontal], 20)
-                
-                Spacer()
-                
-                Button {
-                    
-                } label: {
-                    Image(systemName: "plus.app.fill")
-                        .resizable()
-                        .foregroundColor(.purpleColor)
-                        .frame(width: 48, height: 44)
-                        //.padding([.horizontal], 31)
-
-                }
-
-            }
-            
-            Spacer()
-            
-            Image("assetProvisorio2")
-            
-            Spacer()
-            
-            Text("Suas atividades")
-                .font(.system(size: 20))
-                .fontWeight(.bold)
-                .foregroundColor(.titleColor)
-                .padding([.trailing], 204)
-            
-            
-            ScrollView(.horizontal, showsIndicators: false, content: {
-                
-                HStack {
-                    ForEach($routine) { $routine in
-                        CardView(routine: $routine)
-                    }
-                }
-            })
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, worldi!")
         }
         .padding()
-        .background(Color.gray.opacity(0.18))
-        //.ignoresSafeArea()
-        .navigationBarBackButtonHidden()
-        //.navigationTitle("App")
-        //.navigationBarTitleDisplayMode(.inline)
-        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(routine: .constant(RoutineInfo.datas))
+        ContentView()
     }
 }
